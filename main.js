e9189bb7df7ce92c08bd57f0c78046447ff9fcf3
financially <170222379+financially@users.noreply.github.com>
2024-05-20 12:28:19 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { token, clientId, guildId } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'command test') {
+    await interaction.reply('online');
+  }
+});
+
+client.login(token);
+
+const commands = [
+  {
+    name: 'command test',
+    description: 'online',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
